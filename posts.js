986c7206f794448fa05344edacc6f97aec441ce4
taneeshamadhu18 <youdontdeserveme18@gmail.com>
2024-10-19 12:30:45 +0530

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,6 @@
+ import express from 'express';
+ import { getPosts,createPost } from '../controllers/posts'
+ const router = express.Router();
+ router.get('/',getPosts);
+ router.post('/',createPost);
+ export default router;
